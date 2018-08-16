b718b7a817e4c6d3728d9ecd22467e40021abb19
Joe <badaczewskij@live.com>
2018-08-15 21:47:02 -0400

    adding styles

--- result.php
+++ result.php
@@ -4,7 +4,18 @@ $response = urldecode($_GET['response']);
 ?>
 <html>
 <head>
+<link rel="stylesheet" id="divi-fonts-css" href="http://fonts.googleapis.com/css?family=Open+Sans:300italic,400italic,600italic,700italic,800italic,400,300,600,700,800&amp;subset=latin,latin-ext" type="text/css" media="all">
+<link rel="stylesheet" type="text/css" href="/new-configurator/otherstyles.css">
+<link rel="stylesheet" type="text/css" href="/new-configurator/styles.css">
+<style>
+form {
+    margin-top: 20px !important;
+}
 
+textarea {
+min-height:200px;
+}
+</style>
 </head>
 <body>
 	<script src="http://code.jquery.com/jquery-2.2.4.min.js"
@@ -15,20 +26,19 @@ $response = urldecode($_GET['response']);
 <![endif]-->
 	<script charset="utf-8" type="text/javascript"
 		src="//js.hsforms.net/forms/v2.js"></script>
-	<style>
-textarea {
-	width: 500px !important;
-	height: 200px !important;
-}
-</style>
+	<!--[if lte IE 8]>
+	<script charset="utf-8" type="text/javascript" src="//js.hsforms.net/forms/v2-legacy.js"></script>
+	<![endif]-->
+	<script charset="utf-8" type="text/javascript" src="//js.hsforms.net/forms/v2.js"></script>
 	<script>
-    hbspt.forms.create({
-        portalId: '401673',
-        formId: '240d8cbc-49fd-443a-b5bf-36e1a021e27b',
+	  hbspt.forms.create({
+		portalId: "401673",
+		formId: "6fc67009-76dd-4ea4-aaa5-70c9c9042a62",
+		css: "",
         onFormReady($form, ctx){
             $('textarea[name="message"]').val("<?php echo $response; ?>").change();
         }
-    });
-</script>
+	});
+	</script>
 </body>
 </html>
